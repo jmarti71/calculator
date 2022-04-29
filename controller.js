bfa2ca4b242a08abc7969797fa394c8413e96f8a
Jared <Jared.martin93@Yahoo.com>
2022-04-29 12:56:56 -0500

    add functions for percentages, negative values, and respective buttons

--- controller.js
+++ controller.js
@@ -3,14 +3,17 @@ const displayResult = document.querySelector('.displayResult');
 const valueButtons = document.querySelectorAll('.valBtn');
 const equalBtn = document.querySelector('.equalsBtn');
 const allCearBtn = document.querySelector('.allClearBtn');
+const negativeBtn = document.querySelector('.negativeBtn');
+const percentBtn = document.querySelector('.percentBtn');
 
 let activeError = false;
 let resultValue = '';
-let currentValue = '0';
+let currentValue = '';
 let valueA = 0;
 let operator = '';
 
 function operate (op, numA, numB) {
+    if(numA ==''){numA = '0';}
     const a = parseFloat(numA);
     const b = parseFloat(numB);
 
@@ -68,11 +71,11 @@ function divide (a, b) {
 
 function injectValue (value){
     if(activeError == false) {
-
-        // if we enter an operator
+        // if value is operator
         if(value == '+' || value == '-' || value == '*' || value == '÷' ) {     
             
-            // if select an operator while there is an active operator - execute operate() to obtain value of 1st number set
+            // (string of numbers) if there is an active operator - execute operate() on number set & active operator 
+            // then set operator to new operator
             if ( operator == '+' || operator == '-' || operator == '*' || operator == '÷' ) {
                 operate(operator, valueA, currentValue);
                 operator = value;
@@ -81,12 +84,12 @@ function injectValue (value){
             // if adding to our previous result
             if(typeof(resultValue) == 'number') {
                 console.log("ran result");
-                valueA = resultValue; 
-                displayEntry.textContent = resultValue;
+                valueA = resultValue;           
                 currentValue = ''; //reset
                 console.log(valueA);
                 operator = value;
-                resultValue == 0;    
+                resultValue = '';    
+                displayEntry.textContent = `${valueA}${operator}${currentValue}`;
             }
             else {
                 valueA = currentValue;
@@ -94,19 +97,79 @@ function injectValue (value){
                 currentValue = ''; //reset
                 operator = value;
                 console.log(operator); //also an array?
+                displayEntry.textContent = `${valueA}${operator}${currentValue}`;
             }
         }
-        // if we are entering a number
+
+        // if value is number
         else if (!isNaN(value)) {
+            // if entering number after result, clear and proceed
+            if(typeof(resultValue) == 'number'){
+                clearAll();
+            }
             currentValue = currentValue + value;
             console.log(currentValue);
+            if(operator !== ''){
+                displayEntry.textContent = `${valueA}${operator}${currentValue}`;
+            }
+            else {
+                displayEntry.textContent = currentValue;
+            }
+            console.log(currentValue);          
         }  
-        displayEntry.textContent = displayEntry.textContent + value;
+    }
+}
+
+function percentOperation() {
+    if(currentValue.length >= 1) {
+        // if operating on result
+        if(typeof(resultValue) == 'number') {
+            currentValue = resultValue.toString();
+            resultValue = '';           
+            displayEntry.textContent = `${currentValue}`;          
+        } 
+        displayEntry.textContent = `${currentValue}%`;
+        currentValue = parseFloat(currentValue) / 100;
+        currentValue = Math.round((currentValue + Number.EPSILON) * 100000) / 100000;
+        currentValue = currentValue.toString();
+        if(operator !== ''){
+            displayEntry.textContent = `${valueA}${operator}${currentValue}`;
+        }
+        else {
+            displayResult.textContent = `${currentValue}`;
+        }
+    }
+}
+
+function toggleNegative () {
+    // toggle the result
+    if(typeof(resultValue) == 'number') {
+        currentValue = (resultValue * (-1)).toString();
+        resultValue = '';           
+        displayEntry.textContent = `${currentValue}`;          
+    } 
+    else {
+        // toggle non-result
+        if(currentValue.charAt(0) !== '-') {
+            currentValue = '-'.concat(currentValue);
+        }
+        else if (currentValue.charAt(0) == '-') {
+            console.log('neg detected');
+            currentValue = currentValue.substring(1);
+        }
+
+        // display based on operator status
+        if(operator !== ''){
+            displayEntry.textContent = `${valueA}${operator}${currentValue}`;
+        }
+        else {
+            displayEntry.textContent = currentValue;
+        }
     }
 }
 
 function roundResultResetOp (result) {
-    op = '';
+    operator = '';
     return Math.round((result + Number.EPSILON) * 100000) / 100000;
 }
 
@@ -118,12 +181,14 @@ function clearAll () {
     displayEntry.textContent = '';
     displayResult.textContent = '';
     resultValue = '0';
-    currentValue = '0';
+    currentValue = '';
     valueA = 0;
     operator = '';
     activeError = false;
 }
 
+negativeBtn.addEventListener('click', () =>  { toggleNegative() });
+percentBtn.addEventListener('click', () =>  { percentOperation() });
 allCearBtn.addEventListener('click', () => { clearAll() });
 equalBtn.addEventListener('click', () => { operate(operator, valueA, currentValue) });
 valueButtons.forEach((button) => { button.addEventListener('click', () => {
